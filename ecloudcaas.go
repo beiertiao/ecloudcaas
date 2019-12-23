0d27a0719d3913b510772a3226f5bb8ed1d97ba7
libaol <libaol@123>
2019-12-23 09:55:40 +0800

    change dev branch

--- ecloudcaas.go
+++ ecloudcaas.go
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2019-12-04 13:51:37
+ * @LastEditTime : 2019-12-23 09:54:46
+ * @LastEditors  : Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \undefinedd:\gohome\src\github.com\beiertiao\ecloudcaas\ecloudcaas.go
+ */
 package main
 
 import (
@@ -6,7 +14,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "OK")
+	fmt.Fprintf(w, "branch dev")
 }
 func main() {
 	http.HandleFunc("/", handler)
